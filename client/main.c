5cb8ee3fbd9dcb6aea8f4c099376110a98148285
chaipokoi <chaipokoi@gmail.com>
2016-03-28 20:29:02 +0200

    Ajout de la bonne library, test récupération appareil

--- client/main.c
+++ client/main.c
@@ -1,15 +1,33 @@
 
-#include <libimobiledevice/libimobiledevice.h>
+#include <stdio.h>
 
-#include "talkitive.h"
+#include "/usr/local/include/usbmuxd.h"
+
+
+//#include "talkitive.h"
 
 
 int main()
 {
-	idevice_connection_t socket;
+	usbmuxd_device_info_t *list_devices;
+
+	int res = usbmuxd_get_device_list(&list_devices);
+	if(res < 0)
+	{
+		printf("Veuillez lancer le service usbmuxd\n");
+		return 1;
+	}
+	printf("Appareils trouvé: %d\n", res);
+	if(res == 0)
+	{
+		printf("Aucun appareil trouvé, arrêt.\n");
+		return 1;
+	}
+
+	/*idevice_connection_t socket;
 	idevice_error_t status = talkitive_connect(&socket);	
 	if(status != 0)
 		return 0;
 	talkitive_send_pixel(socket, 0,0,0);
-	return 0;
+	return 0;*/
 }
