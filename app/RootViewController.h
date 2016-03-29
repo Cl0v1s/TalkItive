63eedb2105ff8297a70f9b81396ecbf29a14d131
chaipokoi <chaipokoi@gmail.com>
2016-03-29 20:18:17 +0200

    Avancement dans la réalisation de l'application

--- app/RootViewController.h
+++ app/RootViewController.h
@@ -5,11 +5,18 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <syslog.h>
 
 @interface RootViewController: UIViewController {
 }
 
+@property char* array;
+@property int width;
+@property int height;
+@property FILE *logs;
+
 - (void)listen_socket;
+- (void)draw_array;
 
 
 @end
--- app/RootViewController.mm
+++ app/RootViewController.mm
@@ -3,12 +3,48 @@
 @implementation RootViewController
 - (void)loadView {
 	self.view = [[[UIView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
-	self.view.backgroundColor = [UIColor blackColor];
+	self.view.backgroundColor = [UIColor greenColor];
+
+	//intialisation des propriétés
+	self.width = 320;
+	self.height = 480;
+	self.array = (char*)malloc(self.width*self.height*sizeof(char));
+	int i,u;
+	for(i = 0; i != self.width; i++)
+	{
+		for(u = 0; u != self.height; u++)
+		{
+			self.array[i*self.height+u] = 255;
+		}
+	}
+	self.view.backgroundColor = [UIColor purpleColor];
+
+	self.view.backgroundColor = [UIColor blueColor];
 	[self listen_socket];
 }
 
+- (void)draw_array
+{
+	CGColorSpaceRef colorSpace=CGColorSpaceCreateDeviceRGB();
+    CGContextRef bitmapContext=CGBitmapContextCreate(self.array, self.width, self.height, 8, self.height*self.width, colorSpace,  kCGImageAlphaPremultipliedLast | kCGBitmapByteOrderDefault);
+    CFRelease(colorSpace);
+
+    CGImageRef cgImage=CGBitmapContextCreateImage(bitmapContext);
+    CGContextRelease(bitmapContext);
+
+    UIImage * newimage = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
+    UIImageView * imageView = [[UIImageView alloc] initWithImage:newimage];
+	self.view.backgroundColor = [UIColor whiteColor];
+
+	[self.view addSubview:imageView ];
+	[self.view sendSubviewToBack:imageView ];
+	[self.view setNeedsDisplay];
+	[imageView release]; 
+}
+
 - (void)listen_socket{
-	//cette section est en c
+	//cette section du code est en c
 	int sock;
 	int connection;
 	int len;
@@ -17,6 +53,10 @@
 
 	//création de la structure décrivant le serveur
 	sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	self.view.backgroundColor = [UIColor whiteColor];
+
+
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons(1234);
@@ -24,18 +64,42 @@
 	//bind
 	bind(sock, (struct sockaddr*)&server, sizeof(server));
 
+	self.view.backgroundColor = [UIColor blueColor];
+
+
 	//listen
 	listen(sock, 3);
 
+	self.view.backgroundColor = [UIColor purpleColor];
+
+
 	//signifie qu'on attends les connexions
-	self.view.backgroundColor = [UIColor whiteColor];
 	len = sizeof(struct sockaddr_in);
 	connection = accept(sock, (struct sockaddr*)&client, (socklen_t*)&len);
 
 	if(connection < 0)
 		self.view.backgroundColor = [UIColor redColor];
-	else 
+	else
 		self.view.backgroundColor = [UIColor greenColor];
+
+	//boucle de reception des commandes
+	char data[80];
+	self.view.backgroundColor = [UIColor blueColor];
+
+	recv(connection, &data, 80, MSG_WAITALL);
+
+		//analyse du contenu de la commande
+		if(strstr(data, "pixel") != 0)
+		{
+
+			int x, y, color; 
+	//self.view.backgroundColor = [UIColor purpleColor];
+
+			sscanf(data, "pixel %d %d %d", &x, &y, &color);
+
+			self.array[x*self.width+y] = color;
+			[self draw_array];
+		}
 
 
 
